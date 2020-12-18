30cb5a01383155708d7d50fba7a21ac44b8a4b1a
danivelop <mofg266@gmail.com>
2020-12-18 22:10:31 +0900

    feat: kakao, rakuten api 추가

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+/* External dependencies */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+/* Internal dependencies */
+import App from 'App';
+import * as serviceWorker from 'serviceWorker';
+import 'sanitize.css/sanitize.css';
+
+ReactDOM.render(
+  <HelmetProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>,
+  document.getElementById('root'),
+);
+
+serviceWorker.unregister();
---
+++ src/services/KakaoMapService.ts
@@ -0,0 +1,58 @@
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+interface AddressType {
+  lat: string;
+  lon: string;
+}
+
+class KakaoMapService {
+  private isLoaded: boolean = false;
+
+  constructor() {
+    this.loadScript();
+  }
+
+  private loadScript() {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_KEY}&libraries=services&autoload=false`;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        this.isLoaded = true;
+      });
+    };
+  }
+
+  public async getLatlngFromAddress(address: string): Promise<AddressType> {
+    if (!this.isLoaded) {
+      throw new Error(
+        '카카오 api가 script에 로드되는데 약간의 시간이 걸려요. 바로 호출하지 말아주세요.',
+      );
+    }
+
+    const geocoder = new window.kakao.maps.services.Geocoder();
+
+    const res: AddressType = await new Promise((resolve, reject) => {
+      geocoder.addressSearch(address, (result, status) => {
+        if (status === window.kakao.maps.services.Status.OK) {
+          return resolve({
+            lat: result[0].y,
+            lon: result[0].x,
+          });
+        } else {
+          return reject(status);
+        }
+      });
+    });
+
+    return res;
+  }
+}
+
+export default new KakaoMapService();
