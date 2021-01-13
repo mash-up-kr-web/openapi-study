b87679a3e09baa5f67ba97b1f59f34726aabebb0
kand193 <qkrdudwls99@gmail.com>
2021-01-14 00:02:37 +0900

    wip: 여행 API 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 /* External dependencies */
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import TravelService from './services/TravelService';
 
 function App() {
+  useEffect(() => {
+    (async () => {
+      const res = await TravelService.getSomethingTravel();
+      console.log(res);
+    })();
+  }, []);
+
   return <div>Hello World!</div>;
 }
 
