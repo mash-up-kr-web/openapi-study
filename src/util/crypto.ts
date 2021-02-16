44ab695f3348fde9ff6f39362035178e197d4536
minsour <parkminsoo0128@gmail.com>
2021-02-16 09:55:29 +0900

    Extract key as const

--- src/util/crypto.ts
+++ src/util/crypto.ts
@@ -1,14 +1,16 @@
 import CryptoJS from 'crypto-js';
 
+const KEY = 'goSeaTogether';
+
 export const serialize = (states: any) => {
   const stringified = JSON.stringify(states);
-  const result = CryptoJS.AES.encrypt(stringified, 'secret key 123').toString();
+  const result = CryptoJS.AES.encrypt(stringified, KEY).toString();
 
   return result;
 };
 
 export const deserialize = (encrypted: string) => {
-  const bytes = CryptoJS.AES.decrypt(encrypted, 'secret key 123');
+  const bytes = CryptoJS.AES.decrypt(encrypted, KEY);
   const result = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
   return result;
