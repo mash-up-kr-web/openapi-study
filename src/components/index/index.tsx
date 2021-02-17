c8a0a8a1c773d4920f813fcbe8c3e287ffbb8af1
godjoy <tlschgml556@gmail.com>
2021-02-17 23:51:00 +0900

    add Index contents

---
+++ src/components/index/index.tsx
@@ -0,0 +1,81 @@
+import { Button, Input } from 'antd';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useIndexDispatch } from './IndexContext';
+
+interface IndexProps {
+    isValid: boolean;
+    name: string;
+};
+
+function NameExistComp(props) {
+    const name = props.name;
+    return <>
+        <Title>{name} 님의 초대</Title>;
+        <Link to="/game">
+            <StartButton>🎮start🎮</StartButton>
+        </Link>
+    </>
+}
+
+function NameNotExistComp() {
+    const [value, setValue] = useState('');
+    const dispatch = useIndexDispatch();
+
+    const handleSubmit = () => {
+        dispatch({type: 'SET_NAME', name: value});
+    };
+
+    return <>
+    <form onSubmit={handleSubmit}>
+        <StyledInput 
+            value={value}
+            placeholder="이름을 입력해 주세요."
+            onChange={e => setValue(e.target.value)}/>
+        <br />
+        <Link to="/game">
+            <StartButton onClick={handleSubmit}>🎮start🎮</StartButton>
+        </Link>
+    </form>
+    </>
+}
+
+function Contents({isValid, name}: IndexProps) {
+    if (isValid) {
+        return <NameExistComp name={name}/>
+    } else {
+        return <NameNotExistComp />
+    }
+}
+
+function IndexContents(props: IndexProps) {
+    const isValid = props.isValid;
+    const name = props.name;
+    return <>
+        <Contents isValid={isValid} name={name}/>
+    </>
+}
+
+export default IndexContents;
+
+const Title = styled.div`
+    margin-top: 400px;
+    font-weight: bolder;
+    font-size: 30pt;
+`;
+
+const StyledInput = styled(Input)`
+    margin-top: 400px;
+    width: 200px;
+    height: 50px;
+`;
+
+const StartButton = styled(Button)`
+    margin-top: 20px;
+    width: 200px;
+    height: 50px;
+    font-weight: bolder;
+    font-size: 20pt;
+    text-align: center;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import IndexContents from 'components/index';
 import React from 'react';
+import styled from 'styled-components';
+import OceanBackground from 'components/index/OceanBackground';
+import qs from 'qs';
+const IndexPage = ({location}) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  
+  const data = query.q
+  console.log('name: '+ data);
 
-const IndexPage = () => {
-  return <>Index</>;
+  const isValid = (data === undefined) ? false : true
+  console.log('name isValid: '+ isValid);
+
+  return <>
+    <IndexContainer>
+      <OceanBackground />
+      <IndexContents isValid={isValid} name={data}/>
+    </IndexContainer>
+  </>;
 };
 
 export default IndexPage;
+
+const IndexContainer = styled.div`
+    text-align: center;
+    color: white;
+`;
